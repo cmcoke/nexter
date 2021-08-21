6023f2438b246e2eb8665ff334da8c22473bdf2a
cmcoke <minoru.cm@gmail.com>
2021-08-21 12:57:45 -0500

    created workflow & automation tasks with webpack.js

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,25 @@
+import '../styles/styles.scss'
+import 'lazysizes'
+import 'picturefill'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// allows webpack-dev-server to be executed by allowing hot module replacements and accepts updates on the fly
+if(module.hot) {
+  module.hot.accept()
+}
